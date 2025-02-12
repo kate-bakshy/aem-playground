c321a87b212b69127e8c95f29d542c4407abc9ec
Katsiaryna Bakshy <kate.bakshi@gmail.com>
2025-02-13 00:08:52 +0100

    Add data layer implementation for Comment component (custom implementation)

--- core/src/main/java/com/kb/playground/core/models/Comment.java
+++ core/src/main/java/com/kb/playground/core/models/Comment.java
@@ -1,13 +1,11 @@
 package com.kb.playground.core.models;
 
-import com.adobe.cq.wcm.core.components.models.Component;
-
 /**
  * Defines the {@code Comment} Sling Model used for the {@code /apps/playground/components/comment} component.
  *
  */
 
-public interface Comment extends Component {
+public interface Comment {
 
     /**
      * Retrieves the author name to be displayed.
@@ -24,6 +22,23 @@ public interface Comment extends Component {
      * @return the comment text to be displayed, or {@code null} if no value can be returned
      */
     default String getText() {
+        return null;
+    }
+    
+    default String getId() {
+        return null;
+    }
+    
+    default boolean isEmpty() {
+        return true;
+    }
+    
+    /**
+     * Retrieves the comment text to be displayed.
+     *
+     * @return the comment text to be displayed, or {@code null} if no value can be returned
+     */
+    default String getData() {
         return null;
     }
 }
--- core/src/main/java/com/kb/playground/core/models/impl/CommentImpl.java
+++ core/src/main/java/com/kb/playground/core/models/impl/CommentImpl.java
@@ -1,20 +1,26 @@
 package com.kb.playground.core.models.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
-import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
-import com.adobe.cq.wcm.core.components.models.datalayer.builder.DataLayerBuilder;
-import com.adobe.cq.wcm.core.components.util.AbstractComponentImpl;
-import com.drew.lang.annotations.NotNull;
+import com.adobe.cq.wcm.core.components.util.ComponentUtils;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.components.ComponentContext;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.kb.playground.core.models.Comment;
 
 @Model(
@@ -26,7 +32,9 @@ import com.kb.playground.core.models.Comment;
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class CommentImpl implements Comment {
     
-    protected static final String RESOURCE_TYPE = "/apps/playground/components/comment";
+    protected static final String RESOURCE_TYPE = "playground/components/comment";
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommentImpl.class);
     
     @ValueMapValue
     private String authorName;
@@ -34,14 +42,57 @@ public class CommentImpl implements Comment {
     @ValueMapValue
     private String text;
     
+    @ValueMapValue
+    private String id;
+    
     @Self
     private Resource resource;
+    
+    @ScriptVariable
+    private Page currentPage;
+    
+    @ScriptVariable
+    private ComponentContext componentContext;
     
     public String getAuthorName() {
         return authorName;
     }
-
+    
     public String getText() {
         return text;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public boolean isEmpty() {
+        return StringUtils.isBlank(authorName) && StringUtils.isBlank(text);
+    }
+    
+    public String getData() {
+        String data = null;
+        
+        if (ComponentUtils.isDataLayerEnabled(resource)) {
+            Map<String, Object> dataProperties = new HashMap<String, Object>();
+            dataProperties.put("@type", resource.getResourceType());
+            dataProperties.put("authorName", this.getAuthorName());
+            dataProperties.put("commentText", this.getText());
+            
+            LOGGER.error("Data layer step 1. data properties - " + dataProperties.toString());
+            
+            String componentID = ComponentUtils.getId(resource, this.currentPage,this.componentContext);
+            
+            LOGGER.error("Data layer step 2. component ID - " + componentID);
+            
+            try {
+                data = String.format("{\"%s\":%s}", componentID, new ObjectMapper().writeValueAsString(dataProperties));
+                LOGGER.error("Data layer step 3. Final data - " + data);
+            } catch (JsonProcessingException e) {
+                LOGGER.error("Error during generation of data layer value", e);
+            }
+        }
+        
+        return data;
     }
 }
